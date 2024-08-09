307be4f637b0093252fa0aaf1e8c90b8741dccfa
Sivalakshmiee <sivalakshmie2002@gmail.com>
2024-08-09 16:48:07 +0530

    authentication

--- student/portal/views.py
+++ student/portal/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+
+def signup(request):
+    return render(request, "signup.html")
+
+def handlelogin(request):
+    return render(request, "handlelogin.html")
