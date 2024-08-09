90dc03f64ede2741d356638b78548f2fc6882bea
Sivalakshmiee <sivalakshmie2002@gmail.com>
2024-08-09 17:36:23 +0530

    authentication-with-validation

--- student/portal/urls.py
+++ student/portal/urls.py
@@ -4,5 +4,6 @@ from portal import views
 urlpatterns = [
     path('',views.home, name = "home"),
     path('signup',views.signup,name="signup"),
-    path('login',views.handlelogin, name="handlelogin")
+    path('login',views.handlelogin, name="handlelogin"),
+    path('logout', views.handleLogout, name="handleLogout"),
 ]
